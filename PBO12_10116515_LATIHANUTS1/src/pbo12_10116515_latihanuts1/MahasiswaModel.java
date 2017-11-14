47275f00dd704407c58ea04da8ed5b7c953b9758
ridwan0797 <32489960+ridwan0797@users.noreply.github.com>
2017-11-14 22:31:02 +0700

    Add files via upload

---
+++ PBO12_10116515_LATIHANUTS1/src/pbo12_10116515_latihanuts1/MahasiswaModel.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12_10116515_latihanuts1;
+
+/**
+ *
+ * @author JARVIS PC
+ */
+public class MahasiswaModel {
+    public String nama;
+   public String nim;
+   
+   public MahasiswaModel(String nama ,String nim){
+       this.nama=nama;
+       this.nim=nim;   }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+    
+}
+
